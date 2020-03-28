c8ced7224a6d79729fffff7835eb92f00b514026
tony-chenjy <tony.chenjy@foxmail.com>
2020-03-28 10:25:10 +0800

    initialization

---
+++ src/main/java/org/tony/carrentalservice/api/HelloServiceApi.java
@@ -0,0 +1,19 @@
+package org.tony.carrentalservice.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author tony.chenjy
+ * @date 2020-03-28
+ */
+@RestController
+public class HelloServiceApi {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+}
---
+++ src/test/java/org/tony/carrentalservice/CarRentalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tony.carrentalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRentalServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
